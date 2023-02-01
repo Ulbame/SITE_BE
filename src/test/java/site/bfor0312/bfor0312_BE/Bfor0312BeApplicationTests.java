d59fa24db3b5089aea1e5cc363a063faf6e504d7
choi taeho <bfor0312@gmail.com>
2023-02-01 17:46:52 +0900

    first commit

---
+++ src/test/java/site/bfor0312/bfor0312_BE/Bfor0312BeApplicationTests.java
@@ -0,0 +1,13 @@
+package site.bfor0312.bfor0312_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bfor0312BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
